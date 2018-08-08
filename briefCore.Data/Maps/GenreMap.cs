803afb8cc23191c89960999cb91acd0bee0fd7b4
Petro Knysh <peteknysh@gmail.com>
2018-08-09 00:17:39 +0300

    adding required files for new entities

--- briefCore.Data/Maps/GenreMap.cs
+++ briefCore.Data/Maps/GenreMap.cs
@@ -1,13 +1,16 @@
 ﻿namespace briefCore.Data.Maps
 {
     using Library.Entities;
+    using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
     public class GenreMap
     {
         public GenreMap(EntityTypeBuilder<Genre> builder)
         {
-            
+            builder.ToTable("genres");
+
+            builder.HasKey(g => g.Id);
         }
     }
 }
--- briefCore.Library/Entities/Profiles/GenreProfile.cs
+++ briefCore.Library/Entities/Profiles/GenreProfile.cs
@@ -5,8 +5,7 @@
     public class GenreProfile : Profile
     {
         public GenreProfile()
-        {
-            
+        {    
         }    
     }
 }
