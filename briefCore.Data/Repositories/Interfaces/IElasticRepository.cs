2c346539d9ee0604c622422106ff1804dcda5f97
Petro Knysh <peteknysh@gmail.com>
2018-10-16 03:32:51 +0300

    ongoing work with ES repository

---
+++ briefCore.Data/Repositories/Interfaces/IElasticRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace briefCore.Data.Repositories.Interfaces
+{
+    using System.Threading.Tasks;
+    using Nest;
+
+    public interface IElasticRepository
+    {
+        Task<IndexResponse> Index<TEntity>(ElasticClient client,
+            TEntity entity,
+            string index) where TEntity : class;
+    }
+}
